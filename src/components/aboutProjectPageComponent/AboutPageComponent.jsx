7133bace86a9e4b6e19788d6cb6efd3b132f78d5
Therenegadee <melkindaniil@yandex.ru>
2023-12-26 11:57:29 +0300

    changes in about page

--- src/components/aboutProjectPageComponent/AboutPageComponent.jsx
+++ src/components/aboutProjectPageComponent/AboutPageComponent.jsx
@@ -1,74 +1,7 @@
-// import React, { useState } from 'react';
-// import leftArrow from '../../assets/icons/service/arrow-left.svg';
-// import rightArrow from '../../assets/icons/service/arrow-right.svg';
-// import './AboutPage.css';
-
-// const storiesData = [
-//   {
-//     id: 1,
-//     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac odio vel libero vulputate tincidunt',
-//     imgUrl: 'https://s3.cointelegraph.com/storage/uploads/view/cd2d036d05a5e489fc009d17649dba25.png',
-//   },
-//   {
-//     id: 2,
-//     text: 'Meet the Team',
-//     imgUrl: 'url_to_image_2.jpg',
-//   },
-// ];
-
-// const AboutPage = () => {
-//   const [currentStory, setCurrentStory] = useState(0);
-
-//   const handlePrev = () => {
-//     setCurrentStory((prev) => (prev === 0 ? storiesData.length - 1 : prev - 1));
-//   };
-
-//   const handleNext = () => {
-//     setCurrentStory((prev) => (prev === storiesData.length - 1 ? 0 : prev + 1));
-//   };
-
-//   const handlePreviewClick = (index) => {
-//     setCurrentStory(index);
-//   };
-
-//   return (
-//     <div className="about-page">
-//       <div className="instagram-stories">
-//         <div className="prev-story" onClick={() => handlePreviewClick(currentStory === 0 ? storiesData.length - 1 : currentStory - 1)}>
-//           <img src={storiesData[currentStory === 0 ? storiesData.length - 1 : currentStory - 1].imgUrl} alt="Previous Story" />
-//           <p>{storiesData[currentStory === 0 ? storiesData.length - 1 : currentStory - 1].text}</p>
-//         </div>
-//         <div className="story-container">
-//           <img src={storiesData[currentStory].imgUrl} alt={`Story ${currentStory + 1}`} />
-//           <p>{storiesData[currentStory].text}</p>
-//         </div>
-//         <div className="next-story" onClick={() => handlePreviewClick(currentStory === storiesData.length - 1 ? 0 : currentStory + 1)}>
-//           <img src={storiesData[currentStory === storiesData.length - 1 ? 0 : currentStory + 1].imgUrl} alt="Next Story" />
-//           <p>{storiesData[currentStory === storiesData.length - 1 ? 0 : currentStory + 1].text}</p>
-//         </div>
-//         <button className="arrow-button left-arrow" onClick={handlePrev}>
-//           <img src={leftArrow} alt="Previous" />
-//         </button>
-//         <button className="arrow-button right-arrow" onClick={handleNext}>
-//           <img src={rightArrow} alt="Next" />
-//         </button>
-//       </div>
-//       <div className="about-text">
-//         <h1>Welcome to Our Project</h1>
-//         <p>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac odio vel libero vulputate tincidunt.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AboutPage;
-
-
 import React, { useState } from 'react';
 import leftArrow from '../../assets/icons/service/arrow-left.svg';
 import rightArrow from '../../assets/icons/service/arrow-right.svg';
+import targetIcon from '../../assets/icons/service/target.svg';
 import './AboutPage.css';
 
 const storiesData = [
@@ -81,6 +14,11 @@ const storiesData = [
     id: 2,
     text: 'Meet the Team',
     imgUrl: 'https://s3.cointelegraph.com/storage/uploads/view/3e5d3532c653ad07876294b3af35e4c4.png',
+  },
+  {
+    id: 3,
+    text: 'Ochumet',
+    imgUrl: 'https://s3.cointelegraph.com/storage/uploads/view/0bd13cdf67395593f39ae8371eb8b8a3.png',
   },
 ];
 
@@ -101,7 +39,18 @@ const AboutPage = () => {
 
   return (
     <div className="about-page">
-      <div className="instagram-stories">
+        <div className="about-text">
+            <h1>
+                <img src={targetIcon} alt="Target Icon" className="icon" />
+                Цель проекта
+            </h1>
+                <p>
+                Данный проект был создан для того, чтобы дать возможность людям,
+                не имеющим навыков программирования, собирать данные с веб-сайтов
+                для их целей (коммерческих, исследовательских, личных и т.д.)
+                </p>
+        </div>
+      <div className="stories">
         <div
           className="prev-story"
           onClick={() => handlePreviewClick(currentStory === 0 ? storiesData.length - 1 : currentStory - 1)}
@@ -126,12 +75,6 @@ const AboutPage = () => {
          <button className="arrow-button right-arrow" onClick={handleNext}>
            <img src={rightArrow} alt="Next" />
          </button>
-      </div>
-      <div className="about-text">
-        <h1>Welcome to Our Project</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac odio vel libero vulputate tincidunt.
-        </p>
       </div>
     </div>
   );
